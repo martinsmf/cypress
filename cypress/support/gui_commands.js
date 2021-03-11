c14ecdc7fe40f4fe7269abd6a3f0316f69cfb62f
Matheus F Martins <martinsmf00@gmail.com>
2021-03-10 23:38:47 -0300

    init

---
+++ cypress/support/gui_commands.js
@@ -0,0 +1,9 @@
+/// <reference types="Cypress" />
+
+Cypress.Commands.add('login', () => {
+    cy.visit('user/sign_in')
+
+    cy.get('[data-qa-selector="login_field"]').type(Cypress.env('user_name'))
+    cy.get('#user_password').type(Cypress.env('user_password'))
+    cy.get('[data-qa-selector="sign_in_button"]').click()
+})
