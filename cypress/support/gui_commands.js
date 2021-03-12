d9ba7ab7ab7576d39c1ab40253f69cf650b88af0
Matheus F Martins <martinsmf00@gmail.com>
2021-03-11 22:16:01 -0300

    Criação de projeto implementada

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -7,3 +7,18 @@ Cypress.Commands.add('login', () => {
     cy.get('#user_password').type(Cypress.env('user_password'))
     cy.get('[data-qa-selector="sign_in_button"]').click()
 })
+
+Cypress.Commands.add('logout', () => {
+    cy.get('.qa-user-avatar').click()
+    cy.get('[data-qa-selector="sign_out_link"]').click()
+})
+
+Cypress.Commands.add('gui_createProject', project => {
+
+    cy.visit('projects/new')
+
+    cy.get('#project_name').type(project.nome)
+    cy.get('#project_description').type(project.descirpiton)
+    cy.get('.qa-initialize-with-readme-checkbox').check()    
+    cy.contains('Create project').click()
+})
